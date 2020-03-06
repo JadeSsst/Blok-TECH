73d270c894f0203e6c67c4b255b9026d135d2638
JadeSsst <jadetje@gmail.com>
2020-03-06 13:48:59 +0100

    changes

--- Backend/datingapp/server.js
+++ Backend/datingapp/server.js
@@ -1,16 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 1900
+const slug = require('slug')
+const bodyParser = require('body-parser')
+const mySearch = [];
 
 app.use('/static', express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.post('/add-movie', addMovie);
+app.get('/movie/:id', movie);
+
+
+function movie(req, res) {
+  const id = req.params.id;
+  //filtert de array en zoekt het item dat de id matcht
+  const movie = mySearch.filter(item => item.id === id) [0];
+  res.render('result.ejs', movie)
+}
+//functie zodat movie wordt toegevoegd
+function addMovie(req, res) {
+  console.log(req.body);
+  var id = slug(req.body.geslacht).toLowerCase()
+
+  mySearch.push({
+  id: id, //altijd nodig, id is van de server wel
+  geslacht: req.body.geslacht,
+  leeftijd: req.body.leeftijd,
+  afstand: req.body.afstand,
+  roken: req.body.roken,
+  kinderen: req.body.kinderen,
+  lengte: req.body.lengte,
+  })
+
+  res.redirect('/movie/' + id)
+}
+
+
 
 //test voor de opdracht zonder dynamic data
 //app.get('/', (req, res) => res.render('index.ejs'));
+
 app.get('/', (req, res) => res.render('index.ejs', {title: 'Goedemiddag welkom jongens'}));
 app.get('/error', (req, res) => res.status(404).render('index.ejs', {title: 'foutcode jongens'}));
-
 //app.get('/', (req, res) => res.send('Joooo Jade!!!'))
 app.get('/about', (req, res) => res.send('Dingen ABOUT me'))
 app.get('/static', (req, res) => res.send())
