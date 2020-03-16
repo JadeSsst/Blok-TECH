5dd9063de11dbe6060bb00fcb9b475734cb4c28b
JadeSsst <jadetje@gmail.com>
2020-03-16 21:21:50 +0100

    update

--- Backend/datingapp/server.js
+++ Backend/datingapp/server.js
@@ -1,55 +1,38 @@
+// zorgt ervoor dat variable uit .env beschikbaar zijn
+require('dotenv').config();
+// constante variabelen aanmaken
 const express = require('express')
 const app = express()
 const port = 1900
 const slug = require('slug')
 const bodyParser = require('body-parser')
 const mySearch = [];
-
-// zorgt ervoor dat variable uit .env beschikbaar zijn
-require('dotenv').config();
-var db;
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
-//var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT; // even hier laten voor de test
-
 const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@datingapp-jarbx.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(url, { useNewUrlParser: true });
+var db;
+var session = require('express-session')
 
-// functie om met de database te connecten. maakt een variable db aan
+// functie om met de database te connecten
 client.connect((err, client) => {
   if (err) {
     console.log(err);
   }
   db = client.db(process.env.DB_NAME);
-
-  // db.collection('profile').find().toArray(function (err, data){
-  //   console.log(data);
-  // }
-
-  //client.close();
 });
 
-
+// functie om de aangeklikte filteropties die in de database gestopt zijn, weer terug te renderen op een de pagina
 function movie(req, res) {
-  // profile = await db.collection('profile').findOne({_id: ObjectId(req.params.id)});
-
-  // console.log('PROFILE:', profile);
-
-  // db.collection('profile').find().toArray(function (err, data) {
-  //   const id = req.params.id
-  //   const profile = data.filter(item => item.id === id)[0];
-  //   res.render('result.ejs', profile);
-  // })
-
   db.collection('profile').find(new ObjectID(req.params.id)).toArray(function (err, data) {
-    console.log('DATA:', data);
+    console.log(data);
     res.render('result.ejs', data[0]);
   })
 }
 
-//functie zodat movie wordt toegevoegd
+// functie zodat de geselecteerde filteropties worden onthouden en in een database collection worden gestopt
 function addMovie(req, res) {
-
+  // const id = slug(req.body.username).toLowerCase()
   const result = db.collection('profile').insertOne({
     geslacht: req.body.geslacht,
     leeftijd: req.body.leeftijd,
@@ -62,25 +45,21 @@ function addMovie(req, res) {
   });
 }
 
-
-
-//test voor de opdracht zonder dynamic data
-//app.get('/', (req, res) => res.render('index.ejs'));
-
+// routes bepalen + static folders + views voor ejs
 app.use('/static', express.static(__dirname + '/static'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET
+  }))
+
 app.post('/add-movie', addMovie);
 app.get('/movie/:id', movie);
-
-app.get('/', (req, res) => res.render('index.ejs', {title: 'Goedemiddag welkom jongens'}));
-app.get('/error', (req, res) => res.status(404).render('index.ejs', {title: 'foutcode jongens'}));
-//app.get('/', (req, res) => res.send('Joooo Jade!!!'))
-app.get('/about', (req, res) => res.send('Dingen ABOUT me'))
-app.get('/static', (req, res) => res.send())
-app.get('/image', (req, res) => res.sendFile(__dirname + '/static/images/' + 'strand.jpg'))
-app.get('/mp3', (req, res) => res.sendFile('/Users/Jade/Documents/School/CMD/Jaar2/Blok3/Backend-development/datingapp/horse.mp3'))
-app.get('*', (req, res) => res.send('404 ERROR NOT FOUND'))
+app.get('/', (req, res) => res.render('index.ejs'));
+app.get('*', (req, res) => res.send('404 ERROR NOT FOUND - made by meeeeee'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
