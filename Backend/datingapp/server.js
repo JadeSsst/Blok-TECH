96ed29e460ab006a8416eb7de975fa726fd2e462
JadeSsst <jadetje@gmail.com>
2020-03-16 12:16:37 +0100

    update

--- Backend/datingapp/server.js
+++ Backend/datingapp/server.js
@@ -5,42 +5,74 @@ const slug = require('slug')
 const bodyParser = require('body-parser')
 const mySearch = [];
 
-app.use('/static', express.static(__dirname + '/static'));
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.post('/add-movie', addMovie);
-app.get('/movie/:id', movie);
+// zorgt ervoor dat variable uit .env beschikbaar zijn
+require('dotenv').config();
+var db;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+//var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT; // even hier laten voor de test
+
+const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@datingapp-jarbx.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// functie om met de database te connecten. maakt een variable db aan
+client.connect((err, client) => {
+  if (err) {
+    console.log(err);
+  }
+  db = client.db(process.env.DB_NAME);
+
+  // db.collection('profile').find().toArray(function (err, data){
+  //   console.log(data);
+  // }
+
+  //client.close();
+});
 
 
 function movie(req, res) {
-  const id = req.params.id;
-  //filtert de array en zoekt het item dat de id matcht
-  const movie = mySearch.filter(item => item.id === id) [0];
-  res.render('result.ejs', movie)
+  // profile = await db.collection('profile').findOne({_id: ObjectId(req.params.id)});
+
+  // console.log('PROFILE:', profile);
+
+  // db.collection('profile').find().toArray(function (err, data) {
+  //   const id = req.params.id
+  //   const profile = data.filter(item => item.id === id)[0];
+  //   res.render('result.ejs', profile);
+  // })
+
+  db.collection('profile').find(new ObjectID(req.params.id)).toArray(function (err, data) {
+    console.log('DATA:', data);
+    res.render('result.ejs', data[0]);
+  })
 }
+
 //functie zodat movie wordt toegevoegd
 function addMovie(req, res) {
-  console.log(req.body);
-  var id = slug(req.body.geslacht).toLowerCase()
 
-  mySearch.push({
-  id: id, //altijd nodig, id is van de server wel
-  geslacht: req.body.geslacht,
-  leeftijd: req.body.leeftijd,
-  afstand: req.body.afstand,
-  roken: req.body.roken,
-  kinderen: req.body.kinderen,
-  lengte: req.body.lengte,
-  })
-
-  res.redirect('/movie/' + id)
+  const result = db.collection('profile').insertOne({
+    geslacht: req.body.geslacht,
+    leeftijd: req.body.leeftijd,
+    afstand: req.body.afstand,
+    roken: req.body.roken,
+    kinderen: req.body.kinderen,
+    lengte: req.body.lengte,
+  }, function (err, result) {
+    res.redirect('/movie/' + result.insertedId);
+  });
 }
 
 
 
 //test voor de opdracht zonder dynamic data
 //app.get('/', (req, res) => res.render('index.ejs'));
+
+app.use('/static', express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.post('/add-movie', addMovie);
+app.get('/movie/:id', movie);
 
 app.get('/', (req, res) => res.render('index.ejs', {title: 'Goedemiddag welkom jongens'}));
 app.get('/error', (req, res) => res.status(404).render('index.ejs', {title: 'foutcode jongens'}));
