cd612ee5b3e2a792c206903d90208269a499881d
JadeSsst <jadetje@gmail.com>
2020-03-19 17:03:21 +0100

    update

---
+++ js/feature.js
@@ -0,0 +1,7 @@
+const sliders = document.querySelectorAll('.sliders');
+
+Array.prototype.forEach.call(sliders,(slider) =>{
+  slider.querySelector('input').addEventListener('input', (event) =>{
+    slider.querySelector('span').innerHTML = event.target.value;
+  });
+});
