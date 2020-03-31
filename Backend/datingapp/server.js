47c0498b312291022d1f28f36d6f471dccb7bcfb
JadeSsst <jadetje@gmail.com>
2020-03-31 11:15:46 +0200

    update

--- Backend/datingapp/server.js
+++ Backend/datingapp/server.js
@@ -25,16 +25,16 @@ client.connect((err, client) => {
 
 // functie om de aangeklikte filteropties die in de database gestopt zijn, weer terug te renderen op een nieuwe pagina
 function results(req, res) {
-  db.collection('filters').find(new ObjectID(req.params.id)).toArray(function (err, data) {
+  db.collection('filtersTwee').find(new ObjectID(req.params.id)).toArray(function (err, data) {
     console.log(req.params.id);
     console.log(data);
-    res.render('result.ejs', data[0]);
+    res.render('result.ejs', {id: req.params.id, data: data[0]});
   })
 }
 
 // functie zodat de geselecteerde filteropties worden onthouden en in een database collection worden gestopt
 function addFilter(req, res) {
-  const result = db.collection('filters').insertOne({
+  const result = db.collection('filtersTwee').insertOne({
     geslacht: req.body.geslacht,
     leeftijd: req.body.leeftijd,
     afstand: req.body.afstand,
@@ -46,39 +46,41 @@ function addFilter(req, res) {
   });
 }
 
-app.post('/update', hoi);
-// // zorgt ervoor dat het formulier verzonden wordt en redirect naar andere pagina
-function hoi(req, res) {
-  console.log("gepost?");
-  console.log(req.params.id);
-  res.redirect('/update-filters/');
-}
-
-app.get('/update-filters/', update);
-// zorgt ervoor dat de filters aanpassen pagina verschijnt
+app.get('/adjust-filters/:id', update);
+// functie die wanneer er op 'pas aan' geklikt wordt, de update pagina laat zien met de juiste id in de url
 function update(req, res) {
-  res.render('update.ejs');
+  res.render('update.ejs', {id: req.params.id});
   console.log(req.params.id);
-  console.log("naar andere pagina");
 }
 
-app.post('/update-filters', findAndUpdateData);
+app.post('/update-filters/:id', findAndUpdateData);
 // functie die ervoor zorgt dat juiste id wordt gevonden en die dan de functie updateData aanroept
 function findAndUpdateData(req, res) {
-  db.collection('filters').find(new ObjectID(req.params.id),(err,result) => {
-    console.log(req.params.id);
+  console.log("HEX 24 " + req.params.id);
+  db.collection('filtersTwee').findOne(
+   {_id: ObjectID(req.params.id)},
+    (err, result) => {
     if (err) throw err;
-    const id= result._id;
-    updateData(req, res, () => {
-      console.log('nieuwe geupdate resultaten zijn er!');
-      res.render('result.ejs', data[0]);
-    })
+    const id = result._id;                            t
+
+    updateData(id, () => {
+      res.redirect('/movie/' + id);
+    });
   })
 }
 
+// function findAndUpdateData(req, res) {
+//   db.collection('filters').find(new ObjectID(req.params.id),(err,result) => {
+//     console.log(req.params.id);
+//     if (err) throw err;
+//     const id= result._id;
+//     res.redirect('/movie/' + id);
+//   })
+// }
+
 // functie die de nieuwe info koppelt aan de data en update
 function updateData(req, res) {
-  db.collection('filters').updateOne(
+  db.collection('filtersTwee').updateOne(
     {_id: ObjectID(req.params.id)},
     { $set: {
       geslacht: req.body.geslacht,
@@ -91,34 +93,6 @@ function updateData(req, res) {
         if (err) throw err;
   })
 }
-
-// // voorbeeld
-// app.get('/update-data', findAndUpdateData);
-// //voorbeeld
-// function findAndUpdateData(req, res) {
-//   db.collection('test').findOne(
-//     { name: 'randu' },                //property waar je op zoekt
-//       (err, result) => {                               // mongodb callback
-//       if (err) throw err;
-//       const id = result._id;                            // id van object die we net hebben aangemaakt
-//
-//       updateData(id, () => {
-//         res.status(200).send('Yay geupdate');
-//       });
-//     })
-//   }
-//
-// function updateData(id, callback) {
-//   // voorbeeld voor als je met formulier doet: const data = req.body.data
-//   db.collection('test').updateOne(    // van mongodb
-//   {_id: ObjectID(id) },               // 1. waar je op zoekt
-//   { $set: {title: 'bye!' } },         // 2. property dieje wil aanpassen via set
-//   (err, result) => {                  // 3.
-//     if (err) throw err;
-//
-//     callback()
-//   })
-// }
 
 // routes bepalen + static folders + views voor ejs
 app.use('/static', express.static(__dirname + '/static'));
